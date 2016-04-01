bae938e60d7c6b29da652a413854ef20885912ff
weldermarcosxd <weldermarcosxd@gmail.com>
2016-04-01 00:53:22 -0300

    dependencies

--- app/.tmp/app/app.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('ngFullstackNewApp', ['ngFullstackNewApp.constants', 'ngCookies', 'ngResource', 'ngSanitize', 'ui.router', 'ui.bootstrap']).config(function ($urlRouterProvider, $locationProvider) {
-  $urlRouterProvider.otherwise('/');
-
-  $locationProvider.html5Mode(true);
-});
-//# sourceMappingURL=app.js.map
--- app/.tmp/components/util/util.module.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-
-angular.module('ngFullstackNewApp.util', []);
-//# sourceMappingURL=util.module.js.map
